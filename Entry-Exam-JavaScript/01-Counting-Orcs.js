e2de3737c49c62f17fb3689131d105f0b83f2a7a
Demian Vassilev <demian.vassilev@gmail.com>
2024-06-21 16:13:00 +0300

    more versions w/o loop

--- Entry-Exam-JavaScript/01-Counting-Orcs.js
+++ Entry-Exam-JavaScript/01-Counting-Orcs.js
@@ -3,14 +3,42 @@ function countingOrcs(n, m, h) {
     let initialCount = n;
     let increaseOfCount = m;
     let numberOfHours = h;
-    let totalCount = 0;
 
+    // version-1
+    let totalCount = initialCount;
+    let fightsCount = initialCount;
+    for (let i = 1; i < numberOfHours; i++) {
+        fightsCount += increaseOfCount;
+        totalCount += fightsCount;
+    }
+    console.log(totalCount);
+
+    // version-2
+    totalCount = 0;
     for (let i = 0; i < numberOfHours; i++) {
         totalCount += initialCount + increaseOfCount * i;
     }
-
     console.log(totalCount);
 
+    // version-3
+    totalCount = 0;
+    totalCount += (numberOfHours / 2) * (2 * initialCount + (numberOfHours - 1) * increaseOfCount);
+    console.log(totalCount);
+
+    // version-4
+    totalCount = initialCount * numberOfHours;
+    totalCount += increaseOfCount * (numberOfHours * (numberOfHours - 1)) / 2;
+    console.log(totalCount);
+
+    // version-5
+    totalCount = initialCount;
+    totalCount += initialCount + increaseOfCount;
+    if (numberOfHours > 2) {
+        totalCount += initialCount + increaseOfCount * (numberOfHours - 1);
+    } else if (numberOfHours === 1) {
+        totalCount = initialCount;
+    }
+    console.log(totalCount);
 }
 
 countingOrcs(5, 2, 3);                // 21
